16f7a76a63b77e5f0189a74d78bc4d77b2b9bad3
Simarjeet Kaur <simarjeet518h@gmail.com>
2022-04-26 22:06:41 -0700

    signin folder added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import Home from "./Routes/Home/Home.component.jsx";
 import Navigation from "./Routes/Navigation/Navigation.js";
+import SignIn from "./Routes/sign-in/SignIn.js";
 import { Route, Routes } from "react-router-dom";
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Navigation />}>
-        <Route index element={<Home />}></Route>
+        <Route index element={<Home />} />
+        <Route path="/signin" element={<SignIn />} />
       </Route>
     </Routes>
   );
